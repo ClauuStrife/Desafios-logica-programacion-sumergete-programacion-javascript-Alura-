fdb0360ab243fc420d018bde8acd3f36f075bebb
ClauuStrife <storm.clauu@gmail.com>
2024-07-04 18:02:18 -0600

    Add files via upload

---
+++ Desafio 1/Ejercicio 6 alerta de error/app.js
@@ -0,0 +1,3 @@
+// Muestra una alerta con el texto "¡Error! Completa todos los campos".
+
+alert('¡Error! Completa todos los campos');
---
+++ Desafio 1/Ejercicio 8 promp variable nombre/app.js
@@ -0,0 +1,5 @@
+// Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre 
+
+let nombre = prompt('Escribe tu nombre de usuario');
+
+console.log(nombre);
