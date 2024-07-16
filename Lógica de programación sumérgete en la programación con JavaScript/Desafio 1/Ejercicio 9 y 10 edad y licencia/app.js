8722ff0dca93a2787d88d4483c1d101a71495dbc
Clauus <storm.clauu@gmail.com>
2024-07-16 13:04:56 -0600

    Add files via upload

---
+++ Lógica de programación sumérgete en la programación con JavaScript/Desafio 1/Ejercicio 9 y 10 edad y licencia/app.js
@@ -0,0 +1,14 @@
+/* 9. Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+10. Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".
+*/
+
+let edad = prompt("Ingresa tu edad, porfavor: ");
+
+console.log(edad);
+
+if (edad >= 18) {
+alert("¡Puedes obtener tu licencia de conducir!");
+}
+
+
+
---
+++ Lógica de programación sumérgete en la programación con JavaScript/Desafio 1/Ejericio 1 al 5/app.js
@@ -0,0 +1,20 @@
+// 1.Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
+
+alert('¡Bienvenida y bienvenido a nuestro sitio web!');
+
+// 2.Declara una variable llamada nombre y asígnale el valor "Luna".
+
+let nombre = "Luna";
+
+// 3.Crea una variable llamada edad y asígnale el valor 25.
+
+let edad = 25;
+
+// 4.Establece una variable numeroDeVentas y asígnale el valor 50.
+
+let numeroDeVentas = 50;
+
+// 5. Establece una variable saldoDisponible y asígnale el valor 1000.
+
+let saldoDisponible = 100;
+
