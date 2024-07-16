fb7f18ed887aff7e4b7dad2e72c34a3492a0db15
Clauus <storm.clauu@gmail.com>
2024-07-16 13:14:22 -0600

    Delete Lógica de programación sumérgete en la programación con JavaScript/Desafio 1/Desafio 2 directory

--- Lógica de programación sumérgete en la programación con JavaScript/Desafio 1/Desafio 2/Ejercicio 5 bienvenido usuario/app.js
+++
@@ -1,7 +0,0 @@
-// Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
-
-let nombreUsuario = prompt("Escribe aqui tu nombre, porfavor: ");
-
-console.log(nombreUsuario);
-
-alert(`¡Bienvenida o bienvenido a nuestra pagina, ${nombreUsuario}!`);
