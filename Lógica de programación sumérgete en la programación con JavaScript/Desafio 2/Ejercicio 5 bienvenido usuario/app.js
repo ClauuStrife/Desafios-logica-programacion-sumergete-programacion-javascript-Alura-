dfb756c422be3236293599dd7e410e2f29fa9310
Clauus <storm.clauu@gmail.com>
2024-07-16 13:40:49 -0600

    Add files via upload

---
+++ Lógica de programación sumérgete en la programación con JavaScript/Desafio 2/Ejercicio 5 bienvenido usuario/app.js
@@ -0,0 +1,7 @@
+// Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
+
+let nombreUsuario = prompt("Escribe aqui tu nombre, porfavor: ");
+
+console.log(nombreUsuario);
+
+alert(`¡Bienvenida o bienvenido a nuestra pagina, ${nombreUsuario}!`);
---
+++ Lógica de programación sumérgete en la programación con JavaScript/Desafio 4/Ejercicios 1 a 5/app.js
@@ -0,0 +1,35 @@
+// 1.Crea un programa que utilice console.log para mostrar un mensaje de bienvenida.
+
+console.log("¡Bienvenido a nuesta página!");
+
+// 2.Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza console.log para mostrar el mensaje "¡Hola, [tu nombre]!" en la consola del navegador.
+
+let nombre = "Claudia";
+
+console.log (`¡Hola, ${nombre}!`);
+
+// 3.Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza alert para mostrar el mensaje "¡Hola, [tu nombre]!".
+
+alert(`¡Hola, ${nombre}!`);
+
+/* 4.Utiliza prompt y haz la siguiente pregunta: ¿Cuál es el lenguaje de programación que más te gusta?. 
+Luego, almacena la respuesta en una variable y muestra la respuesta en la consola del navegador.
+*/ 
+
+let lenguajeFavorito = prompt('¿Cuál es el lenguaje de programación que más te gusta?');
+
+console.log(`Su lenguaje favorito es: ${lenguajeFavorito}`);
+
+/*
+5. Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. 
+Luego, realiza la suma de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". 
+Utiliza console.log para mostrar el mensaje "La suma de [valor1] y [valor2] es igual a [resultado]." en la consola.
+*/
+
+let valor1 = 4;
+let valor2 = 6;
+let resultado = valor1 + valor2;
+
+console.log (`La suma de ${valor1} y ${valor2} es igual a ${resultado}`);
+
+
