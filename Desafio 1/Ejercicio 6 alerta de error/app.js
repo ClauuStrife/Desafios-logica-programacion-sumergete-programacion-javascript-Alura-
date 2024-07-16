4833eb56b2bc1663eecd314e4d6a6861bdfe1841
Clauus <storm.clauu@gmail.com>
2024-07-16 12:58:49 -0600

    Delete Desafio 1 directory

--- Desafio 1/Ejercicio 6 alerta de error/app.js
+++
@@ -1,3 +0,0 @@
-// Muestra una alerta con el texto "¡Error! Completa todos los campos".
-
-alert('¡Error! Completa todos los campos');
--- Desafio 1/Ejercicio 8 promp variable nombre/app.js
+++
@@ -1,5 +0,0 @@
-// Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre 
-
-let nombre = prompt('Escribe tu nombre de usuario');
-
-console.log(nombre);
