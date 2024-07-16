91d1c70f221206ad7d346b1cd20e26ca984e6ce0
Clauus <storm.clauu@gmail.com>
2024-07-16 12:59:32 -0600

    Delete Desafio 3 directory

--- Desafio 3/Ejercicio 2 contador 10 a 0/app.js
+++
@@ -1,8 +0,0 @@
-//Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
-
-let contador = 10;
-
-while (contador >= 0) {
-console.log('Ejecutando contador: '+ contador);
-contador = contador - 1;
-}
--- Desafio 3/Ejericicio 1 contador 1 al 10/app.js
+++
@@ -1,8 +0,0 @@
-// Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
-
-let contador = 1;
-
-while (contador <= 10) {
-console.log('Ejecutando contador'+ contador);
-contador = contador + 1;
-}
